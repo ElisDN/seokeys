467f892b4d12d9a304da3df44d7be7a81686a334
ElisDN <mail@elisdn.ru>
2014-11-08 11:18:23 +0300

    Fixed user login

--- modules/user/models/User.php
+++ modules/user/models/User.php
@@ -146,7 +146,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findByUsername($username)
     {
-        return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+        return static::findOne(['username' => $username]);
     }
 
     /**
