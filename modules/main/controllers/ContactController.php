706af530f63601db61670e20c750d8b42a06f000
ElisDN <mail@elisdn.ru>
2015-04-08 20:36:11 +0300

    Added contact form autofill

--- modules/main/controllers/ContactController.php
+++ modules/main/controllers/ContactController.php
@@ -21,9 +21,13 @@ class ContactController extends Controller
     public function actionIndex()
     {
         $model = new ContactForm();
+        if ($user = Yii::$app->user->identity) {
+            /** @var \app\modules\user\models\User $user */
+            $model->name = $user->username;
+            $model->email = $user->email;
+        }
         if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
             Yii::$app->session->setFlash('contactFormSubmitted');
-
             return $this->refresh();
         } else {
             return $this->render('index', [
