c6bd4db463013657548c4e98c39df2f680ac907a
ElisDN <mail@elisdn.ru>
2015-06-26 13:02:55 +0300

    Generated admin users CRUD

---
+++ modules/admin/views/users/_search.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\UserSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'created_at') ?>
+
+    <?= $form->field($model, 'updated_at') ?>
+
+    <?= $form->field($model, 'username') ?>
+
+    <?= $form->field($model, 'auth_key') ?>
+
+    <?php // echo $form->field($model, 'email_confirm_token') ?>
+
+    <?php // echo $form->field($model, 'password_hash') ?>
+
+    <?php // echo $form->field($model, 'password_reset_token') ?>
+
+    <?php // echo $form->field($model, 'email') ?>
+
+    <?php // echo $form->field($model, 'status') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
