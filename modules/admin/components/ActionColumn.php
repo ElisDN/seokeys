f1ca64910f7d7b414ea99ab03b910293dfa77806
ElisDN <mail@elisdn.ru>
2015-06-26 14:31:00 +0300

    Customized action column

---
+++ modules/admin/components/ActionColumn.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\modules\admin\components;
+
+class ActionColumn extends \yii\grid\ActionColumn
+{
+    public $contentOptions = [
+        'class' => 'action-column',
+    ];
+}
