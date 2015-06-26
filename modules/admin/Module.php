7475688218115055dea36f47fad05e8a72d5751b
ElisDN <mail@elisdn.ru>
2015-06-26 15:07:38 +0300

    Added admin module access control

--- modules/admin/Module.php
+++ modules/admin/Module.php
@@ -2,9 +2,26 @@
 
 namespace app\modules\admin;
 
+use yii\filters\AccessControl;
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'app\modules\admin\controllers';
+
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
 
     public function init()
     {
