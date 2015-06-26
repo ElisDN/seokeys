292e02dd459477a16d017677c52dc4293a13554d
ElisDN <mail@elisdn.ru>
2015-06-26 15:04:04 +0300

    Changed admin index page

--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -1,12 +1,17 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model \app\modules\admin\models\User */
+
+$this->title = Yii::t('app', 'ADMIN');
+$this->params['breadcrumbs'][] = $this->title;
+?>
 <div class="admin-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
+
     <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
+        <?= Html::a(Yii::t('app', 'ADMIN_USERS'), ['users/index'], ['class' => 'btn btn-primary']) ?>
     </p>
 </div>
