840e82f01cc0e7065ea248300fa4afeb540b4631
ElisDN <mail@elisdn.ru>
2015-07-10 12:15:45 +0300

    Changed module url rules

--- config/common.php
+++ config/common.php
@@ -38,8 +38,8 @@ return [
 				'<_a:error>' => 'main/default/<_a>',
 				'<_a:(login|logout|signup|confirm-email|request-password-reset|reset-password)>' => 'user/default/<_a>',
 
-                '<_m:[\w\-]+>/<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/<_a>',
                 '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
+                '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
                 '<_m:[\w\-]+>' => '<_m>/default/index',
                 '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
 			],
