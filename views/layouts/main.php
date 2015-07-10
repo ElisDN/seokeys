48755afa5f581a44aab3d0e95c3730304de6c3c2
ElisDN <mail@elisdn.ru>
2015-07-10 11:29:06 +0300

    Compact menu profile items

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -48,25 +48,25 @@ AppAsset::register($this);
                 ['label' => Yii::t('app', 'NAV_LOGIN'), 'url' => ['/user/default/login']] :
                 false,
             !Yii::$app->user->isGuest ?
-                ['label' => Yii::t('app', 'NAV_PROFILE'), 'url' => ['/user/profile/index']] :
-                false,
-            !Yii::$app->user->isGuest ?
                 ['label' => Yii::t('app', 'NAV_ADMIN'), 'items' => [
                     ['label' => Yii::t('app', 'NAV_ADMIN'), 'url' => ['/admin/default/index']],
                     ['label' => Yii::t('app', 'ADMIN_USERS'), 'url' => ['/admin/users/index']],
                 ]] :
                 false,
-            !Yii::$app->user->isGuest ? (
-                '<li>'
-                . Html::beginForm(['/user/default/logout'], 'post')
-                . Html::submitButton(
-                    Yii::t('app', 'NAV_LOGOUT'),
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            ) : false,
-        ]),
+            !Yii::$app->user->isGuest ?
+                ['label' => Yii::t('app', 'NAV_PROFILE'), 'items' => [
+                    ['label' => Yii::t('app', 'NAV_PROFILE'), 'url' => ['/user/profile/index']],
+                    '<li>'
+                    . Html::beginForm(['/user/default/logout'], 'post')
+                    . Html::submitButton(
+                        Yii::t('app', 'NAV_LOGOUT'),
+                        ['class' => 'btn btn-link logout']
+                    )
+                    . Html::endForm()
+                    . '</li>'
+                ]] :
+                false,
+        ],
     ]);
     NavBar::end();
     ?>
