faf29baddeec1d70d1e10a8b8e8ece12c304d03d
ElisDN <mail@elisdn.ru>
2015-07-22 11:08:04 +0300

    updated tests directory

--- tests/codeception/config/config.php
+++ tests/codeception/config/config.php
@@ -3,6 +3,15 @@
  * Application configuration shared by all test types
  */
 return [
+    'language' => 'en-US',
+    'controllerMap' => [
+        'fixture' => [
+            'class' => 'yii\faker\FixtureController',
+            'fixtureDataPath' => '@tests/codeception/fixtures',
+            'templatePath' => '@tests/codeception/templates',
+            'namespace' => 'tests\codeception\fixtures',
+        ],
+    ],
     'components' => [
         'db' => [
             'dsn' => 'mysql:host=localhost;dbname=yii2_basic_tests',
