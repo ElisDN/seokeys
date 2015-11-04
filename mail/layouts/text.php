7388b89efdf4311373ecd916cd6b957b4a4ef9df
ElisDN <mail@elisdn.ru>
2015-11-04 14:05:36 +0300

    Extended test mail layout

--- mail/layouts/text.php
+++ mail/layouts/text.php
@@ -1,7 +1,11 @@
 <?php
-
 /* @var $this \yii\web\View */
 /* @var $content string */
-
+/* @var $message \yii\mail\MessageInterface */
+/* @var $content string */
 ?>
+<?php $this->beginPage() ?>
+<?php $this->beginBody() ?>
 <?= $content ?>
+<?php $this->endBody() ?>
+<?php $this->endPage() ?>
