94c9488e0a5c872db3be173297702eb909c358e5
ElisDN <mail@elisdn.ru>
2016-06-03 22:32:45 +0300

    Added default controller rules

--- config/common.php
+++ config/common.php
@@ -32,9 +32,11 @@ return [
                     'routePrefix' => 'admin',
                     'rules' => [
                         '' => 'default/index',
+                        '<_m:[\w\-]+>' => '<_m>/default/index',
+                        '<_m:[\w\-]+>/<id:\d+>' => '<_m>/default/view',
+                        '<_m:[\w\-]+>/<id:\d+>/<_a:[\w-]+>' => '<_m>/default/<_a>',
                         '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>' => '<_m>/<_c>/view',
                         '<_m:[\w\-]+>/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_m>/<_c>/<_a>',
-                        '<_m:[\w\-]+>' => '<_m>/default/index',
                         '<_m:[\w\-]+>/<_c:[\w\-]+>' => '<_m>/<_c>/index',
                     ],
                 ],
