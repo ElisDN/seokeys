02dcb4f2e77ce8955baf95c6a6f3c0dff5fa3fdf
ElisDN <mail@elisdn.ru>
2017-11-06 11:23:49 +0300

    Extending test config

---
+++ tests/_config/acceptance.php
@@ -0,0 +1,13 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../config/common.php'),
+    require(__DIR__ . '/../../config/common-local.php'),
+    require(__DIR__ . '/../../config/web.php'),
+    require(__DIR__ . '/../../config/web-local.php'),
+    require(__DIR__ . '/../../config/test.php'),
+    require(__DIR__ . '/../../config/test-local.php'),
+    [
+
+    ]
+);
---
+++ tests/_config/functional.php
@@ -0,0 +1,17 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../config/common.php'),
+    require(__DIR__ . '/../../config/common-local.php'),
+    require(__DIR__ . '/../../config/web.php'),
+    require(__DIR__ . '/../../config/web-local.php'),
+    require(__DIR__ . '/../../config/test.php'),
+    require(__DIR__ . '/../../config/test-local.php'),
+    [
+        'components' => [
+            'request' => [
+                'scriptFile' => dirname(dirname(__DIR__)) . '/web/index-test.php',
+            ],
+        ],
+    ]
+);
